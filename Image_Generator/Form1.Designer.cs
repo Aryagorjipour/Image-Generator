e8ed5e744c87f4862dd6b2f3205743f171c9b32a
Aryagorjipour <arygorji.pour@gmail.com>
2021-08-17 20:07:16 +0430

    Project source

---
+++ Image_Generator/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Image_Generator
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnChooseImage = new System.Windows.Forms.Button();
+            this.picBox = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNames = new System.Windows.Forms.TextBox();
+            this.btnStartGenerate = new System.Windows.Forms.Button();
+            this.OpnFile = new System.Windows.Forms.OpenFileDialog();
+            this.btnFont = new System.Windows.Forms.Button();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.fontDFontFam = new System.Windows.Forms.FontDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.colorDFont = new System.Windows.Forms.ColorDialog();
+            this.flderbrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnChooseImage
+            // 
+            this.btnChooseImage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnChooseImage.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChooseImage.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.btnChooseImage.Location = new System.Drawing.Point(579, 12);
+            this.btnChooseImage.Name = "btnChooseImage";
+            this.btnChooseImage.Size = new System.Drawing.Size(237, 35);
+            this.btnChooseImage.TabIndex = 0;
+            this.btnChooseImage.Text = "عکس خود را انتخاب کنید";
+            this.btnChooseImage.UseVisualStyleBackColor = true;
+            this.btnChooseImage.Click += new System.EventHandler(this.BtnChooseImage_Click);
+            // 
+            // picBox
+            // 
+            this.picBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.picBox.Location = new System.Drawing.Point(410, 106);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(406, 264);
+            this.picBox.TabIndex = 1;
+            this.picBox.TabStop = false;
+            this.picBox.Paint += new System.Windows.Forms.PaintEventHandler(this.PicBox_Paint);
+            this.picBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PicBox_MouseDown);
+            this.picBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PicBox_MouseMove);
+            this.picBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PicBox_MouseUp);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Perpetua Titling MT", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(132, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 32);
+            this.label2.TabIndex = 4;
+            this.label2.Text = ": رنگ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtNames
+            // 
+            this.txtNames.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txtNames.Location = new System.Drawing.Point(12, 106);
+            this.txtNames.Multiline = true;
+            this.txtNames.Name = "txtNames";
+            this.txtNames.Size = new System.Drawing.Size(392, 264);
+            this.txtNames.TabIndex = 5;
+            this.txtNames.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnStartGenerate
+            // 
+            this.btnStartGenerate.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnStartGenerate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStartGenerate.ForeColor = System.Drawing.Color.Black;
+            this.btnStartGenerate.Location = new System.Drawing.Point(263, 376);
+            this.btnStartGenerate.Name = "btnStartGenerate";
+            this.btnStartGenerate.Size = new System.Drawing.Size(291, 35);
+            this.btnStartGenerate.TabIndex = 6;
+            this.btnStartGenerate.Text = "شروع ساخت";
+            this.btnStartGenerate.UseVisualStyleBackColor = true;
+            this.btnStartGenerate.Click += new System.EventHandler(this.BtnStartGenerate_Click);
+            // 
+            // OpnFile
+            // 
+            this.OpnFile.FileName = "Choose Your Image";
+            // 
+            // btnFont
+            // 
+            this.btnFont.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFont.Location = new System.Drawing.Point(13, 12);
+            this.btnFont.Name = "btnFont";
+            this.btnFont.Size = new System.Drawing.Size(113, 41);
+            this.btnFont.TabIndex = 7;
+            this.btnFont.Text = "انتخاب قلم";
+            this.btnFont.UseVisualStyleBackColor = true;
+            this.btnFont.Click += new System.EventHandler(this.BtnFont_Click);
+            // 
+            // btnColor
+            // 
+            this.btnColor.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnColor.Location = new System.Drawing.Point(13, 59);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(113, 41);
+            this.btnColor.TabIndex = 9;
+            this.btnColor.Text = "انتخاب رنگ";
+            this.btnColor.UseVisualStyleBackColor = true;
+            this.btnColor.Click += new System.EventHandler(this.BtnColor_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Perpetua Titling MT", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(132, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(213, 32);
+            this.label1.TabIndex = 10;
+            this.label1.Text = ": انتخاب قلم و اندازه";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 417);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(434, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Copyright © 2020 , all rights reserved. Developed by Arya Gorjipour";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(828, 443);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnColor);
+            this.Controls.Add(this.btnFont);
+            this.Controls.Add(this.btnStartGenerate);
+            this.Controls.Add(this.txtNames);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.picBox);
+            this.Controls.Add(this.btnChooseImage);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(844, 482);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(844, 482);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Image Generator";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChooseImage;
+        private System.Windows.Forms.PictureBox picBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNames;
+        private System.Windows.Forms.Button btnStartGenerate;
+        private System.Windows.Forms.OpenFileDialog OpnFile;
+        private System.Windows.Forms.Button btnFont;
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.FontDialog fontDFontFam;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ColorDialog colorDFont;
+        private System.Windows.Forms.FolderBrowserDialog flderbrowser;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Image_Generator/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Image_Generator
+{
+    public partial class frmMain : Form
+    {
+        string _filename;
+        Point _rectStartPoint;
+        Rectangle _rect = new Rectangle();
+        public Brush SelectionBrush = new SolidBrush(Color.FromArgb(128, 72, 145, 220));
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnChooseImage_Click(object sender, EventArgs e)
+        {
+            OpnFile.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+            if (OpnFile.ShowDialog() == DialogResult.OK)
+            {
+                _filename = OpnFile.FileName;
+
+                picBox.Image = new Bitmap(OpnFile.FileName);
+                picBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
+
+        private void PicBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            _rectStartPoint = e.Location;
+            Invalidate();
+        }
+
+        private void PicBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left)
+                return;
+            var tempEndPoint = e.Location;
+            _rect.Location = new Point(
+                Math.Min(_rectStartPoint.X, tempEndPoint.X),
+                Math.Min(_rectStartPoint.Y, tempEndPoint.Y));
+            _rect.Size = new Size(
+                Math.Abs(_rectStartPoint.X - tempEndPoint.X),
+                Math.Abs(_rectStartPoint.Y - tempEndPoint.Y));
+            picBox.Invalidate();
+        }
+
+        private void PicBox_Paint(object sender, PaintEventArgs e)
+        {
+            if (picBox.Image != null)
+            {
+                if (_rect.Width > 0 && _rect.Height > 0)
+                {
+                    e.Graphics.FillRectangle(SelectionBrush, _rect);
+                }
+            }
+        }
+
+        private void PicBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                if (_rect.Contains(e.Location))
+                {
+                    Debug.WriteLine("Right click");
+                }
+            }
+        }
+
+        private void BtnFont_Click(object sender, EventArgs e)
+        {
+            if (fontDFontFam.ShowDialog() == DialogResult.OK)
+                txtNames.Font = fontDFontFam.Font;
+        }
+
+        private void BtnColor_Click(object sender, EventArgs e)
+        {
+            colorDFont.ShowDialog();
+
+            txtNames.ForeColor = colorDFont.Color;
+        }
+
+        private void BtnStartGenerate_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Brush b = new SolidBrush(colorDFont.Color);
+
+                var x = (_rect.X * picBox.Image.Width) / picBox.Width;
+                var y = (_rect.Y * picBox.Image.Height) / picBox.Height;
+
+                var w = (_rect.Width * picBox.Image.Width) / picBox.Width;
+                var h = (_rect.Height * picBox.Image.Height) / picBox.Height;
+
+
+                //for (int i = 0; i < SName.Length; i++)
+                foreach (string n in txtNames.Text.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    var image = new Bitmap(picBox.Image);
+
+                    var graphics = Graphics.FromImage(image);
+
+
+                    var s = graphics.MeasureString(n, fontDFontFam.Font);
+
+
+                    RectangleF RectF = new RectangleF(x + (w - s.Width), y, w, h);
+
+                    graphics.DrawString(n, fontDFontFam.Font, b, RectF);
+
+                    image.Save($"{folderBrowserDialog1.SelectedPath}\\{n}.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+                }
+
+                MessageBox.Show("Done");
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            fontDFontFam.Font = new Font("TimesNewRoman", 25);
+            colorDFont.Color = Color.Black;
+        }
+    }
+}
